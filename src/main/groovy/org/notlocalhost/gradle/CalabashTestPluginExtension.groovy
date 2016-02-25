10aa1366f05b500156389af184c21b5100e6cb77
baoti <yed.liu@gmail.com>
2016-02-25 19:29:12 +0800

    Add working dir extension

--- src/main/groovy/org/notlocalhost/gradle/CalabashTestPluginExtension.groovy
+++ src/main/groovy/org/notlocalhost/gradle/CalabashTestPluginExtension.groovy
@@ -4,6 +4,8 @@ package org.notlocalhost.gradle
  * Created by zan on 25/04/2014.
  */
 class CalabashTestPluginExtension {
+    // Default: project.rootDir
+    String workingDir
     String featuresPath
     String profile
     String[] formats
